df2e6affe9275e8ed5b81d9b4b9a36fb167d240e
Lee Youjin <kynhun20@gachon.ac.kr>
2022-12-28 07:04:36 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ,
+        body: , //카테고리 생성 후 넣을 부분
+        bottomNavigationBar: ,
+      )
+    );
+  }
+}
+
